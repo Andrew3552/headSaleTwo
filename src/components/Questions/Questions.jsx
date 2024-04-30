064b03cdbf71d3eda07742cc868d2e485e5181c1
Andrii <126251484+Andrew3552@users.noreply.github.com>
2024-04-30 08:38:31 +0300

    asd

--- src/components/Questions/Questions.jsx
+++ src/components/Questions/Questions.jsx
@@ -1,4 +1,5 @@
 import { questionsArr } from './questionArr';
+import Container from '../WrapperList/Container';
 
 import QuestionsCard from './QuestionsCard';
 import BtnBanner from '../Bunner/BtnBanner/BtnBanner';
@@ -6,21 +7,28 @@ import BtnBanner from '../Bunner/BtnBanner/BtnBanner';
 import './Questions.scss';
 export default function Questions() {
   return (
-    <div className="questions__wrapper">
-      <h2 className="questions__title">ВІДПОВІДІ НА ЗАПИТАННЯ</h2>
-      <ul className="questions__list">
-        {questionsArr.map((item, index) => (
-          <QuestionsCard data={item} cuntNumber={index + 1} key={index}></QuestionsCard>
-        ))}
-      </ul>
-      <button className="select__tariff"
-        onClick={() => {
-          const element = document.getElementById('tariffs');
-          element.scrollIntoView({ behavior: 'smooth' });
-        }}
-      >
-        Обрати тариф
-      </button>
+    <div className="questions">
+      <Container>
+        <div className='questions__container'>
+          <div className="questions__wrapper">
+            <h2 className="questions__title">ВІДПОВІДІ НА ЗАПИТАННЯ</h2>
+            <ul className="questions__list">
+              {questionsArr.map((item, index) => (
+                <QuestionsCard data={item} cuntNumber={index + 1} key={index}></QuestionsCard>
+              ))}
+            </ul>
+          </div>
+          <button
+            className="select__tariff"
+            onClick={() => {
+              const element = document.getElementById('tariffs');
+              element.scrollIntoView({ behavior: 'smooth' });
+            }}
+          >
+            Обрати тариф
+          </button>
+        </div>
+      </Container>
     </div>
   );
 }
